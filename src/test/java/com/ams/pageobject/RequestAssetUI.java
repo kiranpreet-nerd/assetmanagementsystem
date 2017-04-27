697160460b04585a307e51866dcfbec208faa1d3
harpreet <harpreet.kaur@nerdapplabs.com>
2017-04-27 14:59:59 +0530

    files renamed

--- src/test/java/com/ams/pageobject/RequestAssetUI.java
+++ src/test/java/com/ams/pageobject/RequestAssetUI.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import com.ams.testsetup.SetUp;
 import com.nerdapplabs.test.TestDataReader;
 
-public class Test_RequestAssetUI extends SetUp {
+public class RequestAssetUI extends SetUp {
 
 	static By emailTextBox = By.name("email");
 	static By passwordTextBox = By.name("password");
