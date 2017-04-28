74309d64ec0123890029abab8aec1de7595a4b25
harpreet <harpreet.kaur@nerdapplabs.com>
2017-04-28 13:18:40 +0530

    modification done

--- src/test/java/com/ams/pageobject/RequestAsset_DropDown.java
+++ src/test/java/com/ams/pageobject/RequestAsset_DropDown.java
@@ -54,13 +54,13 @@ public class RequestAsset_DropDown extends SetUp {
 	public boolean verifyDropDownIsSingleSelect() {
 		try {
 			loginFunction();
+			WebElement assetDrop = driver.findElement(type);
+			Select drop = new Select(assetDrop);
+			if (!(drop.isMultiple())) {
+				return true;
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		WebElement assetDrop = driver.findElement(type);
-		Select drop = new Select(assetDrop);
-		if (!(drop.isMultiple())) {
-			return true;
 		}
 		return false;
 	}
@@ -69,15 +69,15 @@ public class RequestAsset_DropDown extends SetUp {
 	public boolean verifyAssetTypeDropDownCount() {
 		try {
 			loginFunction();
+			WebElement assetDrop = driver.findElement(type);
+			Select drop = new Select(assetDrop);
+			List<WebElement> options = drop.getOptions();
+			int count = options.size();
+			if (count == 4) {
+				return true;
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		WebElement assetDrop = driver.findElement(type);
-		Select drop = new Select(assetDrop);
-		List<WebElement> options = drop.getOptions();
-		int count = options.size();
-		if (count == 4) {
-			return true;
 		}
 		return false;
 	}
@@ -87,53 +87,56 @@ public class RequestAsset_DropDown extends SetUp {
 	public boolean verifyDefaultValueOfTypeDropDown() {
 		try {
 			loginFunction();
+			WebElement assetDrop = driver.findElement(type);
+			Select drop = new Select(assetDrop);
+			List<WebElement> options = drop.getOptions();
+			int count = options.size();
+			for (int i = 0; i < count; i++) {
+				listvalue = options.get(i).getText();
+				if (listvalue.equals("None")) {
+					return true;
+				}
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		WebElement assetDrop = driver.findElement(type);
-		Select drop = new Select(assetDrop);
-		List<WebElement> options = drop.getOptions();
-		int count = options.size();
-		for (int i = 0; i < count; i++) {
-			listvalue = options.get(i).getText();
-			if (listvalue.equals("None")) {
-			return true;
-			}
-			return false;
 		}
 		return false;
 	}
-	
-	// verify to select the option from  "asset type" may impact on another drop-down 
+
+	// verify to select the option from "asset type" may impact on another
+	// drop-down
 	public boolean verifyImpactOnNameDropDown() {
 		try {
 			loginFunction();
+			WebElement assetDrop = driver.findElement(type);
+			Select drop = new Select(assetDrop);
+			drop.selectByVisibleText("Asset");
+			String nameDrop = driver.findElement(name).getAttribute("value");
+			if (!(nameDrop.isEmpty())) {
+				return true;
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		WebElement assetDrop = driver.findElement(type);
-		Select drop = new Select(assetDrop);
-		drop.selectByVisibleText("Asset");
-		String nameDrop = driver.findElement(name).getAttribute("value");
-		if(!(nameDrop.isEmpty())) {
-			return true;
-		}
-		return false;		
+
+		return false;
 	}
-	// verify "None" value of "asset type" does't impact on assetName drop-down 
-		public boolean verifyNoneOptionImpactOnNameDropDown() {
-			try {
-				loginFunction();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+
+	// verify "None" value of "asset type" does't impact on assetName drop-down
+	public boolean verifyNoneOptionImpactOnNameDropDown() {
+		try {
+			loginFunction();
 			WebElement assetDrop = driver.findElement(type);
 			Select drop = new Select(assetDrop);
 			drop.selectByVisibleText("None");
 			String nameDrop = driver.findElement(name).getAttribute("value");
-			if(nameDrop.isEmpty()) {
+			if (nameDrop.isEmpty()) {
 				return true;
 			}
-			return false;		
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
+		return false;
+	}
 }
